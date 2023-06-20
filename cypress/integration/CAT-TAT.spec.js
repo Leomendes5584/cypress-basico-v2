fff61bc2629e392b7fd4464593b5f5ffef38222e
Leomendes5584 <leonardobaskette@gmail.com>
2023-06-20 12:46:16 -0400

    New test add

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -159,5 +159,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     
     it.only('testa a página da política de privacidade de forma independente', function(){
          cy.visit('./src/privacy.html')
+         
+         cy.contains('Talking About Testing').should('be.visible')
     })
 })
